2359b1965fe401a0bbd16acfee445eda4357dec4
森 <951540966@qq.com>
2021-07-23 11:50:10 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Toast, Dialog } from 'vant'
+Dialog.setDefaultOptions({
+    confirmButtonColor: '#0078ff',
+})
 const app = createApp(App)
+app.use(Toast)
+app.use(Dialog)
 app.use(router).mount('#app')
 
 
